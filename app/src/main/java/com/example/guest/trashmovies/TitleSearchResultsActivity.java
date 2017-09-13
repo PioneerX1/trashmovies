6ff74fdf897de87a1ab9734d1147f52fc6c23605
Mick Sexton and saul <mick+saul@>
2017-09-13 10:52:04 -0700

    add api key url after experiementing in postman

--- app/src/main/java/com/example/guest/trashmovies/TitleSearchResultsActivity.java
+++ app/src/main/java/com/example/guest/trashmovies/TitleSearchResultsActivity.java
@@ -1,9 +1,13 @@
 package com.example.guest.trashmovies;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class TitleSearchResultsActivity extends AppCompatActivity {
+
+    Intent oldIntent = getIntent();
+    final String title = oldIntent.getStringExtra("title");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
